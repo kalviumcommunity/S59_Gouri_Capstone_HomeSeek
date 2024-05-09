08ffbaf9a42659deb627463f43fbb1e6dea96be2
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-05-09 18:29:01 +0530

    navbar updated

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Navbar.scss"
+import logo from "../../assets/logo.png"
+import menu from "../../assets/menu.png"
 
 function Navbar() {
   return (
@@ -7,7 +9,7 @@ function Navbar() {
     <nav>
         <div className='left'>
         <a href='/' className='logo'>
-         <img src='' alt=''/>
+         <img src={logo} alt=''/>
           <span>HomeSeek</span>
         </a>
         <a href='/'>Home</a>
@@ -17,7 +19,20 @@ function Navbar() {
         </div>
         <div className='right'>
         <a href='/'>Sign in</a>
+        <a href='/'  className='register'>Sign up</a>
+        <div className='menuIcon'>
+        <img src={menu}/>
+        </div>
+
+        <div className='menu'>
+        <a href='/'>Home</a>
+        <a href='/'>About</a>
+        <a href='/'>Contact</a>
+        <a href='/'>Agents</a>
+        <a href='/'>Sign in</a>
         <a href='/'>Sign up</a>
+        </div>
+
         </div>
     </nav>
     
