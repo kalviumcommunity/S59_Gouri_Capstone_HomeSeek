1d0f71d7ad0ce79a86fa07da5bbd5cce5e915b23
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-05-22 17:58:38 +0530

    main layout

--- Frontend/src/pages/homePage/homepage.jsx
+++ Frontend/src/pages/homePage/homepage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './homePage.scss'
+import bg from "../../assets/bg.png"
 
 function Homepage() {
   return (
-    <div className='homePage'>homepage</div>
+    <div className='homePage'>
+      <div className="textContainer">
+        <h1>YOUR PARTNER IN PROPERTY</h1>
+      </div>
+      <div className="imgContainer">
+        <img src={bg} alt=''></img>
+      </div>
+    </div>
   )
 }
 
