824bbc047191cfa42435c79bb410f75c16c97d68
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-06-07 16:34:54 +0530

    singlePage design and data in dummydata

--- Frontend/src/pages/singlePage/singlePage.jsx
+++ Frontend/src/pages/singlePage/singlePage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./singlePage.scss"
+import Slider from '../../components/slider/Slider'
 
 function SinglePage() {
   return (
-    <div>singlePage</div>
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider/>
+          <div className="info">
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+        </div>
+      </div>
+     <div className="features">
+      <div className="wrapper"></div>
+     </div>
+
+    </div>
   )
 }
 
