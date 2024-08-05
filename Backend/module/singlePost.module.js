cce1d6a5140d7d313cfaf1b3a16f7a10bbb96b5c
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-08-05 15:32:30 +0530

    schema added

---
+++ Backend/module/singlePost.module.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+
+const singlePostSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    images: [{ type: String, required: true }],
+    bedroom: { type: Number, required: true },
+    bathroom: { type: Number, required: true },
+    size: { type: Number, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    city: { type: String, required: true },
+    address: { type: String, required: true },
+    school: { type: String, required: true },
+    bus: { type: String, required: true },
+    restaurant: { type: String, required: true },
+    description: { type: String, required: true }
+});
+
+const SinglePost = mongoose.model('SinglePost', singlePostSchema);
+
+module.exports = SinglePost;
