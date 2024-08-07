e931d221485b49cfffd34e1294e7d8772e6a56b8
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-08-07 17:57:36 +0530

    putrequest added

--- Backend/routes/property.route.js
+++ Backend/routes/property.route.js
@@ -5,7 +5,7 @@ const propertyRouter = express.Router();
 
 propertyRouter.get('/property', async (req, res) => {
   try {
-    const property = await Property.find().select('title');
+    const property = await Property.find();
     res.json({ property });
   } catch (error) {
     res.status(500).json({ error: 'Error fetching property' });
@@ -22,5 +22,18 @@ propertyRouter.post('/addproperty', async (req, res) => {
     res.status(400).send({ message: "Error adding property", error: error.message });
   }
 });
+
+propertyRouter.put('/property/:id', async (req, res) => {
+    try {
+      const updatedProperty = await Property.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!updatedProperty) {
+        return res.status(404).send({ message: "Property not found" });
+      }
+      res.status(200).send({ message: "Property updated successfully", updatedProperty });
+    } catch (error) {
+      console.error(`Error updating property: ${error.message}`, error);
+      res.status(400).send({ message: "Error updating property", error: error.message });
+    }
+  });
 
 module.exports = propertyRouter;
