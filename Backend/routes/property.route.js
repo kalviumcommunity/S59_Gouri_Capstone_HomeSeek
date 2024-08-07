572c96377404ce5019fd70b33634bc394e4a99d7
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-08-07 16:30:43 +0530

    getrequest added

--- Backend/routes/property.route.js
+++ Backend/routes/property.route.js
@@ -1,19 +1,26 @@
-const express=require('express');
-const Property = require('../module/property.module');
-const propertyRouter=express.Router()
+const express = require('express');
+const User = require('../module/userData.module');
+const Property = require('../module/property.module'); 
+const propertyRouter = express.Router();
 
-
-propertyRouter.post('/addproperty',async(req,res)=>{
-    try{
-        const newProperty=new Property(req.body);
-        await newProperty.save();
-        res.status(201).send("Property added Successfully")
-    }
-    catch(error){
-        console.error(`Error adding propert:${error.message}`,error)
-        res.status(400).send({message:"error adding property",error:error.message});
-    }
+propertyRouter.get('/property', async (req, res) => {
+  try {
+    const property = await Property.find().select('title');
+    res.json({ property });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching property' });
+  }
 });
 
+propertyRouter.post('/addproperty', async (req, res) => {
+  try {
+    const newProperty = new Property(req.body);
+    await newProperty.save();
+    res.status(201).send("Property added successfully");
+  } catch (error) {
+    console.error(`Error adding property: ${error.message}`, error);
+    res.status(400).send({ message: "Error adding property", error: error.message });
+  }
+});
 
-module.exports=propertyRouter
+module.exports = propertyRouter;
